f2e6ec8226331941e884c8c5f649a03ecdcb0580
Yegor <yogafeed@gmail.com>
2021-02-21 00:24:19 +0500

    several fixes

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use('/api/private/', require('./routes/privateRoutes'));
 app.use(errorHandler);
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/client/build')));
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     });
